c22160a4fa809c3cb030d0da1528ad1102f02052
Admin <Admin@DESKTOP-SLFGGPN>
2022-09-27 21:13:02 +0530

    First Commit

---
+++ src/gitrepository/git1.java
@@ -0,0 +1,16 @@
+package gitrepository;
+
+public class git1 {
+	
+	public void text() {
+		System.out.println("written text");
+	}
+
+	public static void main(String[] args) {
+		
+git1 git=new git1();
+git.text();
+
+	}
+
+}
