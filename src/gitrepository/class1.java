76911072319e999671862d52bf0ca9c893e28217
Admin <Admin@DESKTOP-SLFGGPN>
2022-09-28 10:53:23 +0530

    second commit

---
+++ src/gitrepository/class1.java
@@ -0,0 +1,19 @@
+package gitrepository;
+
+public class class1 {
+
+		
+		public void text() {
+			System.out.println("written text1");
+		}
+
+		public static void main(String[] args) {
+			
+	class1 git=new class1();
+	git.text();
+
+		}
+
+	}
+
+
